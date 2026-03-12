93cf42024bb80b4148bac81a6420c1230af6be3a
chaitanya <chaitanya.rongali@everest.engineering>
2026-03-13 01:50:25 +0530

    Add email  state to the submit input widget

--- lib/src/widgets/submit_button.dart
+++ lib/src/widgets/submit_button.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 class Submit extends StatelessWidget {
-  const Submit({super.key});
+   final TextEditingController emailController;
+  const Submit({super.key,required this.emailController});
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +10,15 @@ class Submit extends StatelessWidget {
               padding: const EdgeInsets.all(28.0),
               child: ElevatedButton(
                 onPressed: () {
+                   String email = emailController.text;
                    if (kDebugMode) {
-                     print('chiatanya');
+                     print('Login successful for user: $email');
                    }
               
                   showDialog(context: context, builder:(BuildContext context){
                     return AlertDialog(
                       title: Text('Login Success'),
-                      content: Text('You have successfully logged in!'),
+                      content: Text('Welcome $email!\nYou have successfully logged in.'),
                       actions:[
                         TextButton(onPressed:(){
                           Navigator.of(context).pop(); 
