cbb86dcd78197abba522f1928202101bcd7f49ed
chaitanya <chaitanya.rongali@everest.engineering>
2026-04-13 18:20:37 +0530

    Add userdata  from the local storage and logout button funcgtionality

--- lib/src/screens/profile_screen.dart
+++ lib/src/screens/profile_screen.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-class ProfileScreen extends StatelessWidget {
-  final String? email;
- 
+import 'package:shared_preferences/shared_preferences.dart';
 
-  const ProfileScreen({super.key, this.email});
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+class _ProfileScreenState extends State<ProfileScreen> {
+  String? email;
+  String? username;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    loadUser();
+  }
+
+  void loadUser() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      email = prefs.getString('email');
+      username = prefs.getString('username');
+      isLoading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    final displayEmail = email ?? 'Not available';
+    final displayUsername = username ?? 'Not available';
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            "Logged in as:\n$email",
-            textAlign: TextAlign.center,
-          ),
+          const Icon(Icons.person, size: 80),
           const SizedBox(height: 20),
-           ElevatedButton(
-            onPressed: () {
+            Text('Username: $displayUsername'),
+            const SizedBox(height: 20),
+            Text('Email: $displayEmail'),
+           const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              await prefs.clear();
               context.go('/');
             },
-            child: const Text("Logout"),
+            child: const Text('Logout'),
           ),
-        ]
-       
-),
+        ],
+      ),
     );
   }
 }
