47238118718357bdb3293ddd8f0e9b640ae8712d
chaitanya <chaitanya.rongali@everest.engineering>
2026-04-17 18:55:18 +0530

    Add intialize the notification and add navigation based on tap of notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:login_page_flutter/src/core/app_routes.dart';
+import 'package:login_page_flutter/src/core/notification_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); 
+  await NotificationService.notifications.cancelAll();
+  await NotificationService.init((payload) {
+    if (payload == 'home') {
+      navigatorKey.currentContext?.go('/home');
+    } else if (payload == 'profile') {
+      navigatorKey.currentContext?.go('/profile');
+    }
+  });
   runApp(App());
 }
 
